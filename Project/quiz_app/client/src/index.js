1fc74d22cc72ce7753b7ced139387c1babfa3d8a
saurabhpanchal <61181093+saurabhpanchal8950@users.noreply.github.com>
2023-06-09 09:13:45 +0530

    Update index.js

--- Project/quiz_app/client/src/index.js
+++ Project/quiz_app/client/src/index.js
@@ -7,6 +7,64 @@ import App from './components/App';
 /** Redux Store */
 import store from './redux/store';
 import { Provider } from 'react-redux';
+const cors = require('cors')
+app.use(cors({
+  origin:["https://deploy-mern-1whq.vercel.app"],
+  methods:["POST","GET"],
+  credentials: true
+}))
+
+mongoose.connect('"mongodb+srv://admin:saurabh123@quiz.azvribh.mongodb.net/test?retryWrites=true&w=majority"')
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import App from './components/App';
+
+
+/** Redux Store */
+import store from './redux/store';
+import { Provider } from 'react-redux';
+const cors = require('cors')
+app.use(cors({
+  origin:["https://deploy-mern-1whq.vercel.app"],
+  methods:["POST","GET"],
+  credentials: true
+}))
+
+mongoose.connect('"mongodb+srv://admin:saurabh123@quiz.azvribh.mongodb.net/test?retryWrites=true&w=majority"')
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import App from './components/App';
+
+
+/** Redux Store */
+import store from './redux/store';
+import { Provider } from 'react-redux';
+const cors = require('cors')
+app.use(cors({
+  origin:["https://deploy-mern-1whq.vercel.app"],
+  methods:["POST","GET"],
+  credentials: true
+}))
+
+mongoose.connect('"mongodb+srv://admin:saurabh123@quiz.azvribh.mongodb.net/test?retryWrites=true&w=majority"')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
